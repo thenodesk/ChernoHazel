de8188d19328804b440b030ccaeb7988eb255d61
thenodesk <rodrigo_v68@msn.com>
2021-07-18 20:18:13 -0300

    buffers and layout abstraction

--- ChernoHazel/src/Hazel/Application.h
+++ ChernoHazel/src/Hazel/Application.h
@@ -10,7 +10,7 @@
 #include "Hazel/ImGui/ImGuiLayer.h"
 
 #include "Renderer/Shader.h"
-
+#include "Hazel/Renderer/Buffer.h"
 
 namespace Hazel {
 
@@ -38,8 +38,10 @@ namespace Hazel {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		unsigned int m_VertexArray, m_VertexBuffer, m_IndexBuffer;
+		unsigned int m_VertexArray;
 		std::unique_ptr<Shader> m_Shader;
+		std::unique_ptr<VertexBuffer> m_VertexBuffer;
+		std::unique_ptr<IndexBuffer> m_IndexBuffer;
 	private:
 		static Application* s_Instance;
 	};
