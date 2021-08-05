816dffab8976fc1c110955bfcdc73eee5eec4581
thenodesk <rodrigo_v68@msn.com>
2021-08-05 15:42:33 -0300

    scene camera

--- ChernoHazel/src/Hazel/Scene/Components.h
+++ ChernoHazel/src/Hazel/Scene/Components.h
@@ -2,7 +2,7 @@
 
 #include <glm/glm.hpp>
 
-#include "Hazel/Renderer/Camera.h"
+#include "SceneCamera.h"
 
 namespace Hazel {
 
@@ -41,13 +41,12 @@ namespace Hazel {
 
 	struct CameraComponent
 	{
-		Hazel::Camera Camera;
+		SceneCamera Camera;
 		bool Primary = true;
+		bool FixedAspectRation = false;
 
 		CameraComponent() = default;
 		CameraComponent(const CameraComponent&) = default;
-		CameraComponent(const glm::mat4 & projection)
-			: Camera(projection) {}
 	};
 
 }
