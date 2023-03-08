8bb1da3e52d363ce8581aa8723329947c136a32c
thenodesk <rodrigo_v68@msn.com>
2023-03-08 17:48:52 -0300

    debugging C# scripts

--- ChernoHazel/src/Hazel/Scripting/ScriptEngine.cpp
+++ ChernoHazel/src/Hazel/Scripting/ScriptEngine.cpp
@@ -7,6 +7,8 @@
 #include "mono/metadata/assembly.h"
 #include "mono/metadata/debug-helpers.h"
 #include "mono/metadata/attrdefs.h"
+#include "mono/metadata/mono-debug.h"
+#include "mono/metadata/threads.h"
 
 #include "FileWatch.h"
 
@@ -66,7 +68,7 @@ namespace Hazel {
             return buffer;
         }
 
-        static MonoAssembly* LoadMonoAssembly(const std::filesystem::path& assemblyPath)
+        static MonoAssembly* LoadMonoAssembly(const std::filesystem::path& assemblyPath, bool loadPDB = false)
         {
             uint32_t fileSize = 0;
             char* fileData = ReadBytes(assemblyPath, &fileSize);
@@ -83,12 +85,30 @@ namespace Hazel {
                 return nullptr;
             }
 
+            if (loadPDB)
+            {
+                std::filesystem::path pdbPath = assemblyPath;
+                pdbPath.replace_extension(".pdb");
+
+                if (std::filesystem::exists(pdbPath))
+                {
+                    uint32_t pdbFileSize = 0;
+                    char* pdbFileData = ReadBytes(pdbPath, &pdbFileSize);
+                    mono_debug_open_image_from_memory(image, (const mono_byte*)pdbFileData, pdbFileSize);
+                    HZ_CORE_INFO("Loaded PDB {}", pdbPath);
+
+                    delete[] pdbFileData;
+                }
+            }
+
             std::string pathString= assemblyPath.string();
             MonoAssembly* assembly = mono_assembly_load_from_full(image, pathString.c_str(), &status, 0);
             mono_image_close(image);
 
             // Don't forget to free the file data
             delete[] fileData;
+
+            
 
             return assembly;
         }
@@ -149,6 +169,8 @@ namespace Hazel {
 
         Scope<filewatch::FileWatch<std::string>> AppAssemblyFileWatcher;
         bool AssemblyReloadPending = false;
+
+        bool EnableDebugging = true;
 
         // Runtime
         Scene* SceneContext = nullptr;
@@ -235,12 +257,27 @@ namespace Hazel {
     {
         mono_set_assemblies_path("mono/lib");
 
+        if (s_Data->EnableDebugging)
+        {
+            const char* argv[2] = {
+                "--debugger-agent=transport=dt_socket,address=127.0.0.1:2550,server=y,suspend=n,loglevel=3,logfile=MonoDebugger.log",
+                "--soft-breakpoints"
+            };
+
+            mono_jit_parse_options(2, (char**)argv);
+            mono_debug_init(MONO_DEBUG_FORMAT_MONO);
+        }
+
         MonoDomain* rootDomain = mono_jit_init("HazelJITRuntime");
         HZ_CORE_ASSERT(rootDomain);
 
         // Store the root domain pointer
         s_Data->RootDomain = rootDomain;
 
+        if (s_Data->EnableDebugging)
+            mono_debug_domain_create(s_Data->RootDomain);
+
+        mono_thread_set_main(mono_thread_current());
     }
 
     void ScriptEngine::ShutdownMono()
@@ -262,7 +299,7 @@ namespace Hazel {
 
         // Move this maybe
         s_Data->CoreAssemblyFilepath = filepath;
-        s_Data->CoreAssembly = Utils::LoadMonoAssembly(filepath);
+        s_Data->CoreAssembly = Utils::LoadMonoAssembly(filepath, s_Data->EnableDebugging);
         s_Data->CoreAssemblyImage = mono_assembly_get_image(s_Data->CoreAssembly);
         //Utils::PrintAssemblyTypes(s_Data->CoreAssembly);
     }
@@ -271,7 +308,7 @@ namespace Hazel {
     {
         // Move this maybe
         s_Data->AppAssemblyFilepath = filepath;
-        s_Data->AppAssembly = Utils::LoadMonoAssembly(filepath);
+        s_Data->AppAssembly = Utils::LoadMonoAssembly(filepath, s_Data->EnableDebugging);
         s_Data->AppAssemblyImage = mono_assembly_get_image(s_Data->AppAssembly);
         //Utils::PrintAssemblyTypes(s_Data->AppAssembly);
 
@@ -465,7 +502,8 @@ namespace Hazel {
 
     MonoObject* ScriptClass::InvokeMethod(MonoObject* instance, MonoMethod* method, void** params)
     {
-        return mono_runtime_invoke(method, instance, params, nullptr);
+        MonoObject* exception = nullptr;
+        return mono_runtime_invoke(method, instance, params, &exception);
     }
 
     ScriptInstance::ScriptInstance(Ref<ScriptClass> scriptClass, Entity entity)
